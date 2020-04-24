f06648deea54e5b889fbbb54e1c6e2c872d7511c
Lucas Neves <lucas54neves@gmail.com>
2020-04-24 20:08:01 -0300

    Addition of Yarn installation script

---
+++ installation-scripts/install-yarn.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+sudo apt install -y curl
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install -y yarn
